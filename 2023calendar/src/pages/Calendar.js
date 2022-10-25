59c8b2545b00e671b292cb45764186b8270e0a26
lywon725 <lywon725@naver.com>
2022-10-26 04:08:17 +0900

    Calendar 페이지 주문하기 버튼 구현
    로그인 했을때에는 다음 주문페이지로 넘어가고 로그인을 안했을때에는 로그인을 해달라고 콘솔을 띄웠다. 이제 팝업창을 구현해보자

--- 2023calendar/src/pages/Calendar.js
+++ 2023calendar/src/pages/Calendar.js
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useRef, useEffect, useState} from 'react';
 import styled from 'styled-components';
+import {auth, signInWithGoogle} from '../firebase';
 
 const OrderBox = styled.button`
 font-family: 'JejuMyeongjo';
@@ -10,9 +11,35 @@ line-height: 12px;
 color: white;
 `
 function Calendar() {
+    //로그인 확인 함수
+    const [LoginCheck, setCheck] = useState(false);
+    //배열을 가진 useEffect 리렌더링될때 마다 실행됨
+    useEffect(() => {
+        auth.onAuthStateChanged(user => {
+            if (user) {
+                setCheck(true);
+                console.log(LoginCheck);
+            } else {
+                setCheck(false);
+                console.log(LoginCheck);
+            }
+        })
+    }, []);
+
+    //주문하기 버튼 클릭 함수
+    const check= () => {
+        {LoginCheck ? 
+            //로그인 했을때 주문 페이지로 이동
+            window.location.href = '/Order1'
+        : 
+            //로그인 안했을때 
+            console.log("로그인을 해주세요")
+        };
+    }
+    
     return (
         <>
-            <OrderBox onClick={() => (window.location.href = '/Order1')} >주문하기</OrderBox>
+            <OrderBox onClick={() => (check())} >주문하기</OrderBox>
         </>
     )
 }
